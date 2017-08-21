6502cbea43253b4116f288e24281ca2185a1e575
Tim Nibert <docz2a@gmail.com>
2017-08-21 22:11:05 +1000

    just a comment change

--- downloader.py
+++ downloader.py
@@ -7,14 +7,14 @@
 
 import youtube_dl
 
-listurl = ""
+listurl = ""            # replace this with the url of the playlist to download
 
 options = {
   'format': 'bestaudio/best',
   'extractaudio' : True,  # only keep the audio
   'audioformat' : "mp3",  # convert to mp3
   'outtmpl': '%(title)s.mp3',    # name the file the ID of the video
-  'noplaylist' : False,    # only download single song, not playlist
+  'noplaylist' : False,    # download playlist
 }
 
 downloader = youtube_dl.YoutubeDL(options)
